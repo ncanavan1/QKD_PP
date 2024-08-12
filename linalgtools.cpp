65f61f8def3cfd2545a52fc984a8fa4a443c0a49
ncanavan1 <ncanavan815@qub.ac.uk>
2024-08-12 09:56:20 +0100

    nearly done linalg cpp

--- linalgtools.cpp
+++ linalgtools.cpp
@@ -2,6 +2,8 @@
 #include<cmath>
 #include<vector>
 #include <omp.h>
+#include<algorithm>
+#include<set>
 #include <eigen3/Eigen/Dense>
 
 using namespace std;
@@ -14,6 +16,20 @@ class mod2system_solver{
         int n;
         int m;
         Matrix<short,Dynamic,Dynamic> A;
+        vector< vector< int>> pos_arr;
+
+
+        void swap_rows_positional(int i){
+            int minPos = pos_arr[i][0];
+            int minRow = i;
+            for(int k=i; k<n; k++){
+                if (pos_arr[k][0] < minPos){
+                    minPos = pos_arr[k][0];
+                    minRow = k;
+                }
+            }
+            pos_arr[i].swap(pos_arr[minRow]);       
+        }
 
         void swap_rows(int i){
             int maxEl = abs(A(i,i));
@@ -28,6 +44,48 @@ class mod2system_solver{
                 int tmp = A(maxRow,k);
                 A(maxRow,k) = A(i,k);
                 A(i,k) = tmp;
+            }
+        }
+
+        vector<int> find_difference(vector<int> v1, vector<int> v2){
+            vector<int> difference;
+            for(int i =0; i < v1.size(); i++){
+                bool match_v1 = false;
+                for(int j = 0; j < v2.size(); j++){
+                    if(v1[i] == v2[j]){
+                        match_v1 = true;
+                    }
+                }
+                if(match_v1 == false){
+                    difference.push_back(v1[i]);
+                }
+            }
+
+            for(int j =0; j < v2.size(); j++){
+                bool match_v2 = false;
+                for(int i = 0; i < v1.size(); i++){
+                    if(v1[i] == v2[j]){
+                        match_v2 = true;
+                    }
+                }
+                if(match_v2 == false){
+                    difference.push_back(v2[j]);
+                }
+            }
+            sort(difference.begin(), difference.end());
+            return difference;
+        }
+
+        void eliminate_below_positional(int i){
+            #pragma omp parallel for
+            for(int k=i+1; k < n; k++){
+                //if first position in rows matches, conduct elimination. Equal to 1 in lower row in pivot col
+                if(pos_arr[i][0] == pos_arr[k][0]){
+                    vector<int> tmp;
+                    //finding the difference is eqivalent to operating an XOR on the explicit rows
+                    tmp = find_difference(pos_arr[i],pos_arr[k]);
+                    pos_arr[k] = tmp;
+                }
             }
         }
 
@@ -50,6 +108,19 @@ class mod2system_solver{
         }
 
 
+        void eliminate_above_positional(int i){
+            #pragma omp parallel for
+            for (int k=0; k < i; k++){
+                for (int j = 0; j < pos_arr[k].size(); j++){
+                    if (pos_arr[i][0] == pos_arr[k][j]){
+                        vector<int> tmp;
+                        tmp = find_difference(pos_arr[i],pos_arr[k]);
+                        pos_arr[k] = tmp;
+                    }
+                }
+            }
+        }
+
         void eliminate_above(int i, int pivot){
             
             #pragma omp parallel for
@@ -61,6 +132,7 @@ class mod2system_solver{
                 }
             }
         }
+        
 
         int find_pivot_col(int row, int prev_pivot){
 
@@ -74,36 +146,35 @@ class mod2system_solver{
             return next_pivot;
         }
 
-
-        void cut_empty_rows(){
-
-            int last_nonZero_row = -1;
-            for(int i=n-1; i >= 0; i--){
-                bool isZeroRow = true;
-                for(int j = 0; j < m+1; j++){
+        void convert_to_positional(){
+            for(int i = 0; i < n; i++){
+                vector<int> row;
+                for(int j=0; j< m+1; j++){
                     if (A(i,j) == 1){
-                        isZeroRow = false;
-                        last_nonZero_row = i;
-                        break;
+                        row.push_back(j);
                     }
                 }
-                if (isZeroRow == false){
-                    break;
-                }
+                pos_arr.push_back(row);
             }
-
-            Matrix<short, Dynamic, Dynamic> temp(last_nonZero_row+1,m+1);
-            temp = A(seq(0,last_nonZero_row),seq(0,m));
-            A = temp;
-            n = last_nonZero_row;
-
-            print_matrix();
-            
         }
 
+        void prune_empty_rows(){
+                //remove empty rows
+                vector<int> empty_row_index;
+                #pragma omp parallel for
+                for(int j = 0; j < pos_arr.size(); j++){
+                    if(pos_arr[j].size() == 0){
+                        empty_row_index.push_back(j);
+                    }
+                }
+
+                for(int k = 0; k < empty_row_index.size(); k++){
+                    pos_arr.erase(pos_arr.begin() + empty_row_index[k] - k);
+                }
+                n = n - empty_row_index.size();
+        }
 
     public:
-
 
         mod2system_solver(){
         }     
@@ -114,6 +185,25 @@ class mod2system_solver{
             A = a;
             cout << "System Initialised as: " << endl;
             print_matrix();
+            convert_to_positional();
+        }
+
+        void print_positional(){
+            for(int i=0; i<n; i++){
+                for(int j=0;j<m+1;j++){
+                    if (count(pos_arr[i].begin(), pos_arr[i].end(),j) == 1){
+                        cout << "1" << "\t";
+                    }
+                    else{
+                        cout << "0" << "\t";
+                    }
+                    if(j == m-1){
+                        cout << "|";
+                    }
+                }
+                cout << "\n";
+            }
+            cout << endl;
         }
 
         void print_matrix(){
@@ -129,11 +219,37 @@ class mod2system_solver{
             cout << endl;
         }   
 
+
+        void gauss_jordan_elimination_positional(){
+
+            for (int i=0; i<n; i++) {
+                cout << "SWAP" << endl;
+                swap_rows_positional(i);
+                print_positional();
+
+                cout << "Eliminate Below" << endl;
+                eliminate_below_positional(i);
+                print_positional();
+
+                cout << "Eliminate Above" << endl;
+                eliminate_above_positional(i);
+                print_positional();
+
+
+                cout << "After pruning empty rows" << endl;
+                prune_empty_rows();
+                print_positional();
+
+
+            }
+        }
+
+
         void gauss_jordan_elimination() {
 
             int prev_pivot = -1;
             int next_pivot;
-            int final_row = -1;
+            int final_row = n-1;
             
 
             for (int i=0; i<n; i++) {
@@ -172,22 +288,66 @@ class mod2system_solver{
             cout << "RREF finished" << endl;
         }
 
+        vector<vector <int>> binary_combinations(int k){
+
+            vector<vector<int> > combinations;
+            int total_combs = pow(2,k);
+
+            for(int i = 0; i < total_combs; i++){
+                vector<int> comb;
+                for(int j = k-1; j >= 0; j --){
+                    if(i & (1 << j)){
+                        comb.push_back(1);
+                    }
+                    else{
+                        comb.push_back(0);
+                    }
+                }
+                combinations.push_back(comb);
+            }            
+            return combinations;
+        }
+
         Matrix<int,Dynamic,Dynamic> find_solutions(){
             
+            //find free variables, number k
+            //create solution space of 2^k x m
+            //find all bimary combinations of free variables
+            //solve for the resulting solutions
+
+            vector<int> basic_vars;
+            vector<int> free_vars;
+            for(int i = 0; i < n; i++){
+                basic_vars.push_back(pos_arr[i][0]);
+
+                //if not a basic variable, then its a free variable
+                //second condition stops considering b vector as in A matrix
+                if(pos_arr.size() > 1){
+                    for(int j = 1; j < pos_arr.size(); j ++){
+                        if (pos_arr[i][j] != m && count(free_vars.begin(), free_vars.end(), pos_arr[i][j]) == 0){
+                            free_vars.push_back(pos_arr[i][j]);
+                        }
+                    }
+                }
+            }
+
+            
+            vector<vector<int>> bins;
+            bins = binary_combinations(free_vars.size());
         }
         
 };
 
 
 
-int main(){
+int main1(){
 
     int n = 11;
     int m = 6;
 
 
     int lines[11][7] = {
-	{1,1,0,0,0,0,0},
+	{1,1,0,0,0,0,0}, //swap first element to 1
 	{0,0,1,1,0,0,0},
 	{0,0,0,0,1,1,1},
 	{1,0,0,0,0,0,0},
@@ -209,13 +369,14 @@ int main(){
 
     mod2system_solver lin_system;
     lin_system.setSystem(n,m,A);
-    lin_system.gauss_jordan_elimination();
-
+    lin_system.gauss_jordan_elimination_positional();
+    //lin_system.find_solutions();
+    return 0;
 }
 
 
 
-int main1() {
+int main() {
     
 
     int n = 3;
@@ -237,5 +398,7 @@ int main1() {
 
     mod2system_solver lin_system;
     lin_system.setSystem(n,m,A);
-    lin_system.gauss_jordan_elimination();
+    lin_system.gauss_jordan_elimination_positional();
+    lin_system.find_solutions();
+
 }
